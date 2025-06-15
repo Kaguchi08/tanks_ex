371fde4535bddb6e75352f2b31e56b15ff81d9a6
Kaguchi08 <k.sakaguchi.592@gmail.com>
2025-06-16 00:54:14 +0900

    feat: 高度なネットワーク同期のためのShared契約を拡張
    - ShellFireData: 砲弾発射データの完全同期
    - ExplosionForceData: 爆発力・位置・ダメージの同期
    - GameResultData: ゲーム結果とラウンド情報の同期
    - ITankGameHubの新メソッド追加:
      - FireAsync(ShellFireData): 詳細な砲弾データ送信
      - UpdateHealthAsync: HP情報の同期
      - ApplyExplosionForceAsync: 物理力の適用
      - NotifyGameResultAsync: ゲーム結果の通知
    - ITankGameHubReceiverの対応イベント追加
    
    MessagePackによる効率的なシリアライゼーション対応

--- src/tanks.Shared/ITankGameHub.cs
+++ src/tanks.Shared/ITankGameHub.cs
@@ -11,7 +11,10 @@ namespace Tanks.Shared
         Task<bool> JoinAsync(string playerName);
         Task LeaveAsync();
         Task<bool> UpdatePositionAsync(TankPositionData positionData);
-        Task<bool> FireAsync(int playerID);
+        Task<bool> FireAsync(ShellFireData fireData);
+        Task<bool> UpdateHealthAsync(int playerID, float currentHealth);
+        Task<bool> ApplyExplosionForceAsync(ExplosionForceData explosionData);
+        Task<bool> NotifyGameResultAsync(GameResultData gameResult);
     }
 
     // クライアントが実装するインターフェース
@@ -22,7 +25,10 @@ namespace Tanks.Shared
         void OnLeave(int playerID);
         void OnGameStart();
         void OnUpdatePosition(TankPositionData positionData);
-        void OnFire(int playerID);
+        void OnFire(ShellFireData fireData);
+        void OnHealthUpdate(int playerID, float currentHealth);
+        void OnExplosionForce(ExplosionForceData explosionData);
+        void OnGameResult(GameResultData gameResult);
     }
 
     // タンクの位置情報データ
@@ -43,5 +49,74 @@ namespace Tanks.Shared
 
         [Key(4)]
         public float RotationY { get; set; }
+    }
+
+    [MessagePackObject]
+    public class ShellFireData
+    {
+        [Key(0)]
+        public int PlayerID { get; set; }
+
+        [Key(1)]
+        public float PositionX { get; set; }
+
+        [Key(2)]
+        public float PositionY { get; set; }
+
+        [Key(3)]
+        public float PositionZ { get; set; }
+
+        [Key(4)]
+        public float DirectionX { get; set; }
+
+        [Key(5)]
+        public float DirectionY { get; set; }
+
+        [Key(6)]
+        public float DirectionZ { get; set; }
+
+        [Key(7)]
+        public float Force { get; set; }
+    }
+
+    [MessagePackObject]
+    public class ExplosionForceData
+    {
+        [Key(0)]
+        public int TargetPlayerID { get; set; }
+
+        [Key(1)]
+        public float ExplosionX { get; set; }
+
+        [Key(2)]
+        public float ExplosionY { get; set; }
+
+        [Key(3)]
+        public float ExplosionZ { get; set; }
+
+        [Key(4)]
+        public float Force { get; set; }
+
+        [Key(5)]
+        public float Radius { get; set; }
+
+        [Key(6)]
+        public float Damage { get; set; }
+    }
+
+    [MessagePackObject]
+    public class GameResultData
+    {
+        [Key(0)]
+        public int WinnerPlayerID { get; set; }
+
+        [Key(1)]
+        public int RoundNumber { get; set; }
+
+        [Key(2)]
+        public bool IsGameEnd { get; set; }
+
+        [Key(3)]
+        public string WinnerName { get; set; } = string.Empty;
     }
 } 
